7896f0412e5e74d17dd00aa3744bcc14a2847e8b
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-08-28 12:36:22 -0300

    Alterações de user

--- src/routers/route.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next)=>{
-  res.status(200).send("<h1>API para Ferramenta de Projeto</h1>");
-})
-
-module.exports=router;
