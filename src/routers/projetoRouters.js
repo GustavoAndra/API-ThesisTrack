db1eb3d8812317ac34a44d5c2f3ae8c9f89c7870
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-10-01 22:13:02 -0300

    :zap: Ajustes nas rotas

--- src/routers/projetoRouters.js
+++ src/routers/projetoRouters.js
@@ -3,21 +3,25 @@ const projetoController = require('../controllers/projetoController');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 
+// Define um caminho base para todas as rotas deste arquivo
+const basePath = '/projeto';
+
 // Rota para criar um projeto (protegida por autenticação JWT)
-router.post('/projeto/adiciona',  projetoController.criarProjeto);
+router.post(`${basePath}/adiciona`, projetoController.criarProjeto);
 
 // Rota para listar todos os projetos (protegida por autenticação JWT)
-router.get('/projeto/listar', projetoController.listarProjetos);
+router.get(`${basePath}/listar`, projetoController.listarProjetos);
 
 // Rota para listar um projeto por ID (protegida por autenticação JWT)
-router.get('/projeto/listar/:id',  projetoController.listarProjetoPorId);
+router.get(`${basePath}/listar/:id`, projetoController.listarProjetoPorId);
 
-router.get('/lista/orientador' , projetoController.listarTodosProfessores);
+// Rota para listar os professores orientadores dos projetos (protegida por autenticação JWT)
+router.get(`/lista/orientador`, projetoController.listarTodosProfessores);
 
 // Rota para atualizar um projeto por ID (protegida por autenticação JWT)
-router.put('/projeto/atualiza/:id',  projetoController.atualizarProjeto);
+router.put(`${basePath}/atualiza/:id`, projetoController.atualizarProjeto);
 
 // Rota para deletar um projeto por ID (protegida por autenticação JWT)
-router.delete('/projeto/delete/:id',authMiddleware.verifyToken, projetoController.deletarProjeto);
+router.delete(`${basePath}/delete/:id`, authMiddleware.verifyToken, projetoController.deletarProjeto);
 
 module.exports = router;
