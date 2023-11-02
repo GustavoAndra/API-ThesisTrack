24552b612e5edd9b1db5776944d2fc3a0bb82e70
Gustavo <gustavo-adsilva26@educar.rs.gov.br>
2023-11-01 22:43:41 -0300

    :zap: Ajustes nas funções de listagem por id e na rota de listagem, sendo obrigatório passas o parâmetro

--- src/routers/projetoRouters.js
+++ src/routers/projetoRouters.js
@@ -12,8 +12,8 @@ router.get(`${basePath}/listar/`, projetoController.listarProjetos);
 // Rota para listar um projeto por id referenciando seu criador
 router.get(`${basePath}/listar/:projetoId/aluno/:id`, projetoController.listarProjetoPorId);
 
-// Rota para listar os projetos de alunos com base no curso que essas pessoas se encontram
-router.get(`${basePath}/lista/aluno/curso/:id`, projetoController.listarProjetosPorCurso);
+// Rota para listar os projetos de alunos com base no curso que essas pessoas se encontram (protegida por autenticação JWT)
+router.get(`${basePath}/lista/aluno/curso/:id`, authMiddleware.verifyToken, projetoController.listarProjetosPorCurso);
 
 // Rota para criar um projeto (protegida por autenticação JWT)
 router.post(`${basePath}/adiciona`, authMiddleware.verifyToken, projetoController.criarProjeto);
